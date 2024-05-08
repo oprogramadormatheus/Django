05e6d5255e1e62794dba18a7c352e95a5d7816ef
Matheus Oliveira <matheusprogramadorcontato@gmail.com>
2024-05-08 19:22:44 -0300

    QuerySets

--- Receitas/urls.py
+++ Receitas/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'recipes'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('recipes/category/<int:category_id>', views.category, name="category"),
     path('recipes/<int:id>', views.recipes, name="recipe"),
 ]
--- Receitas/views.py
+++ Receitas/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from . import models
 
 def index(request):
-    context  = {'recipes': [make_recipe() for _ in range(10)]}
+
+    recipes = models.Recipe.objects.filter(is_published=True).order_by('-id')
+
+    context  = {'recipes': recipes}
     return render(request, 'receitas/pages/index.html', context)
 
 def recipes(request, id):
 
     context = {'recipe': make_recipe(), 'is_detail_page': True}
     return render(request, 'receitas/pages/recipes.html', context)
+
+def category(request, category_id):
+
+    recipes = models.Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    category_name = models.Category.objects.filter(id=category_id)[0]
+    context  = {'recipes': recipes, 'category_name': category_name}
+    return render(request, 'receitas/pages/category.html', context)
