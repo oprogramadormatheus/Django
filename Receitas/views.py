fbd32e53c1a5316170cd277f182488343b44995d
Matheus Oliveira <matheusprogramadorcontato@gmail.com>
2024-05-13 19:01:58 -0300

    Melhorando Templates

--- Receitas/views.py
+++ Receitas/views.py
@@ -1,28 +1,22 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
 from . import models
-from django.http import Http404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from utils.recipes.factory import make_recipe
 
 def index(request):
 
     recipes = models.Recipe.objects.filter(is_published=True).order_by('-id')
-
     context  = {'recipes': recipes}
     return render(request, 'receitas/pages/index.html', context)
 
 def recipes(request, id):
 
-    context = {'recipe': make_recipe(), 'is_detail_page': True}
+    recipe = models.Recipe.objects.filter(pk=id, is_published=True).first()
+    context = {'recipe': recipe, 'is_detail_page': True}
     return render(request, 'receitas/pages/recipes.html', context)
 
 def category(request, category_id):
-
-    recipes = models.Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-
-    try:
-        category_name = models.Category.objects.filter(id=category_id)[0]
-    except IndexError:
-        raise Http404('Not Found')
-
-    context  = {'recipes': recipes, 'category_name': category_name}
+    
+    recipes = get_list_or_404(models.Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+    category = recipes[0].category.name
+    context  = {'recipes': recipes, 'category': category}
     return render(request, 'receitas/pages/category.html', context)
