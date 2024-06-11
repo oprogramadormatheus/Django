13f50282d5310de2bf5f3df1b546a02dc7bbc859
Matheus Oliveira <matheusprogramadorcontato@gmail.com>
2024-06-11 19:00:41 -0300

    Pulando Testes

--- Receitas/tests/test_recipes_views.py
+++ Receitas/tests/test_recipes_views.py
@@ -1,14 +1,15 @@
+from unittest import skip
 from Receitas import views
 from django.urls import reverse, resolve
 from .test_recipes_base import ReceitasTestBase
 
 class ReceitasViewsTest(ReceitasTestBase):
 
-
+    @skip('WIP')
     def test_recipes_index_view_function_is_correct(self):
         view = resolve(reverse('recipes:index'))
         self.assertIs(view.func, views.index)
-    
+        
     def test_recipes_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
@@ -64,8 +65,22 @@ class ReceitasViewsTest(ReceitasTestBase):
 
     
     def test_recipes_index_loads_recipes(self):
-        self.create_recipe(author_data={'first_name': 'João'}, category_data={'name': 'Café da Manhã'})
+        self.create_recipe(category_data={'name': 'Café da Manhã'})
         response = self.client.get(reverse('recipes:index'))
         content = response.content.decode('utf-8')
-        self.assertIn('João', content)
+        self.assertIn('Matheus', content)
         self.assertIn('Café da Manhã', content)
+    
+    def test_recipes_category_loads_recipes(self):
+        self.create_recipe(category_data={'name': 'Carnes Assadas'})
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1}))
+        content = response.content.decode('utf-8')
+        self.assertIn('Matheus', content)
+        self.assertIn('Carnes Assadas', content)
+    
+    def test_recipes_recipe_loads_recipe(self):
+        self.create_recipe(category_data={'name': 'Jantar'})
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        content = response.content.decode('utf-8')
+        self.assertIn('Matheus', content)
+        self.assertIn('Jantar', content)
