428b2ba26bf2882eff20b4404d17d4aa1c124ad7
Matheus Oliveira <matheusprogramadorcontato@gmail.com>
2024-07-01 19:01:17 -0300

    Testando o Coverage

--- Receitas/tests/test_recipes_models.py
+++ Receitas/tests/test_recipes_models.py
@@ -1,5 +1,5 @@
 from unittest import skip
-from .test_recipes_base import ReceitasTestBase
+from .test_recipes_base import ReceitasTestBase, Recipe
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
@@ -8,8 +8,26 @@ class ReceitasModelsTest(ReceitasTestBase):
     def setUp(self) -> None:
         self.recipe = self.create_recipe()
         return super().setUp()
+    
+    def create_recipe_no_defaults(self):
 
-    @skip('Just a Test')
+        recipe_test = Recipe.objects.create(
+            category = self.create_category(name='Test Default Category'),
+            author = self.create_author(username='test.user'),
+            title = 'Test Title',
+            description = 'Test Description',
+            slug = 'slug-test',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 5,
+            servings_unit = 'Pessoas',
+            preparation_steps = 'Test Preparation Steps',
+        )
+        recipe_test.full_clean()
+        recipe_test.save()
+        return recipe_test
+
+    @skip('WIP')
     def test_recipe_title_field_max_length(self):
         self.recipe.title = 'A' * 70
         with self.assertRaises(ValidationError):
@@ -19,4 +37,12 @@ class ReceitasModelsTest(ReceitasTestBase):
     def test_recipe_fields_max_length(self, field, size):
         setattr(self.recipe, field, 'A' * (size + 1))
         with self.assertRaises(ValidationError):
-            self.recipe.full_clean() # VALIDAÇÃO DO CAMPOS
+            self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_if_false_by_default(self):
+        recipe_test = self.create_recipe_no_defaults()
+        self.assertFalse(recipe_test.preparation_steps_is_html)
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe_test = self.create_recipe_no_defaults()
+        self.assertFalse(recipe_test.is_published)
