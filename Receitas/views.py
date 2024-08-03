67f12db0642c3a4c2447c81993a7493516c37b09
Matheus Oliveira <matheusprogramadorcontato@gmail.com>
2024-08-03 10:56:19 -0300

    Usando .env

--- Receitas/views.py
+++ Receitas/views.py
@@ -1,13 +1,16 @@
+import os
 from . import models
 from django.db.models import Q
 from django.http import Http404
 from .sources.pagination import create_pagination
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
 def index(request):
 
     recipes = get_list_or_404(models.Recipe.objects.filter(is_published=True).order_by('-id'))
-    page_obj, pagination_range = create_pagination(request, recipes, 2)
+    page_obj, pagination_range = create_pagination(request, recipes, PER_PAGE)
 
     context  = {
         'recipes': page_obj,
@@ -25,7 +28,7 @@ def recipes(request, id):
 def category(request, category_id):
     
     recipes = get_list_or_404(models.Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
-    page_obj, pagination_range = create_pagination(request, recipes, 2)
+    page_obj, pagination_range = create_pagination(request, recipes, PER_PAGE)
     category = recipes[0].category.name
     context  = {'recipes': page_obj, 'pagination_range': pagination_range, 'category': category}
     return render(request, 'receitas/pages/category.html', context)
@@ -41,7 +44,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = create_pagination(request, recipes, 2)
+    page_obj, pagination_range = create_pagination(request, recipes, PER_PAGE)
     
     context = {
         'recipes': page_obj,
--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z7)vi$f4hqplki4y=@)@(o4@=)*7_gi+itxz+2ghfr)7z78^3p'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+
+# 'django-insecure-z7)vi$f4hqplki4y=@)@(o4@=)*7_gi+itxz+2ghfr)7z78^3p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = []
 
